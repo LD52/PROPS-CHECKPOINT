07f7d8d40d8cb66a89ec3171e397507f9f8c77c6
unknown <konancurry@gmail.com>
2023-03-02 14:57:23 +0000

    adding checkpoint

---
+++ src/Profile/Profile.js
@@ -0,0 +1,9 @@
+function HandleName ({fullName, bio, profession}){
+   alert(fullName);
+  }
+  function ReactHeader({ version = "16" }) {
+    return <h1>React {version} Documentation</h1>;
+  }
+  
+
+export default HandleName
